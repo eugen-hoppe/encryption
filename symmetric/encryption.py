794c55eca3a1f373a0df86c0e48c593bee46272b
Eugen <eugen.hoppe.de@gmail.com>
2024-04-18 19:22:23 +0200

    refactoring of exception handling

--- symmetric/encryption.py
+++ symmetric/encryption.py
@@ -2,14 +2,9 @@ import os
 
 from typing import Type
 
-from cryptography.exceptions import (
-    InvalidKey,
-    AlreadyFinalized,
-    UnsupportedAlgorithm
-)
-
 from symmetric.interface import SymmetricEncryption, GenericKey, Mode
-from utils.exceptions import ErrTxt
+from utils.exceptions import ErrTxt, TryExceptKeys
+from utils.error_handling import try_except
 
 
 MODE = Mode.PRODUCTION
@@ -18,17 +13,13 @@ DEFAULT_NONCE_OR_PADDING = 16
 DEFAULT_KEY_GENERATION_LENGTH = 32
 DEFAULT_KEY_GENERATION_ITERATIONS = 100_000
 
-ERR_GENENRATE_KEY = "Key generation failed"
-ERR_ENCRYPTION = "Encryption failed"
-ERR_DECRYPTION = "Decryption failed"
-ERR_INVALID_STR = "ERROR: arg_{0} is not a string. Type:{1}"
-
 
 class Key(SymmetricEncryption, GenericKey):
     def __init__(self, algorithm: Type[SymmetricEncryption]):
         self.algorithm: str = algorithm.__name__
         self.core: SymmetricEncryption = algorithm()
 
+    @try_except(**TryExceptKeys.GENERATE_ERROR.kw())
     def generate(
         self,
         pw: str,
@@ -38,39 +29,30 @@ class Key(SymmetricEncryption, GenericKey):
         iterations: int = DEFAULT_KEY_GENERATION_ITERATIONS,
         key_length: bool = DEFAULT_KEY_GENERATION_LENGTH
     ) -> tuple[str, str | None, str | None]:
-        try:
-            ErrTxt.validate_strings(pw, salt)
-            if salt == "":
-                salt = os.urandom(16).hex()
-            return self.core.generate(
-                pw, salt, get_salt, get_pw, iterations, key_length
-            )
-        except (ValueError, TypeError) as e:
-            self.core.raise_value_error(ERR_GENENRATE_KEY, e, MODE)
+        ErrTxt.validate_strings(pw, salt)
+        if salt == "":
+            salt = os.urandom(16).hex()
+        return self.core.generate(
+            pw, salt, get_salt, get_pw, iterations, key_length
+        )
 
+    @try_except(**TryExceptKeys.ENCRYPT_ERROR.kw())
     def encrypt(
             self,
             payload: str,
             key: str,
             size: int = DEFAULT_NONCE_OR_PADDING
         ) -> str:
-        try:
-            ErrTxt.validate_strings(payload, key)
-            return self.core.encrypt(payload, key, size)
-        except (InvalidKey, UnsupportedAlgorithm) as err:
-            self.core.raise_value_error(ERR_ENCRYPTION, err, MODE)
+        ErrTxt.validate_strings(payload, key)
+        return self.core.encrypt(payload, key, size)
 
+
+    @try_except(**TryExceptKeys.DECRYPT_ERROR.kw())
     def decrypt(
             self,
             encrypted: str,
             key: str,
             size: int = DEFAULT_NONCE_OR_PADDING
         ) -> str:
-        try:
-            ErrTxt.validate_strings(encrypted, key)
-            return self.core.decrypt(encrypted, key, size)
-        except (InvalidKey,
-                AlreadyFinalized,
-                UnsupportedAlgorithm,
-                ValueError) as err:
-            self.core.raise_value_error(ERR_DECRYPTION, err, MODE)
+        ErrTxt.validate_strings(encrypted, key)
+        return self.core.decrypt(encrypted, key, size)
