08636560d47af1b61566478c9af9853cb75c2502
Eugen <eugen.hoppe.de@gmail.com>
2024-04-19 01:34:51 +0200

    hybrid encryption

--- example.py
+++ example.py
@@ -1,18 +1,25 @@
 from tests.asymmetric import example_101
 from tests.symmetric import example_201, example_202
+from tests.hybrid import example_301
 
 
 TEST_IDS = [
-    101,  # 201,
-    202,
+    # 101,  # 201,
+    # 202,
+    301,
 ]
+
 
 
 if __name__ == "__main__":
 
     if 101 in TEST_IDS:
         example_101.run_test()
+
     if 201 in TEST_IDS:
         example_201.run_test()
     if 202 in TEST_IDS:
         example_202.run_test()
+
+    if 301 in TEST_IDS:
+        example_301.run_test()
---
+++ tests/hybrid/example_301.py
@@ -0,0 +1,26 @@
+from core.asymmetric.encryption import Keys
+from core.symmetric.encryption import Key
+from core.algorithms.aes import AES256
+from core.algorithms.rsa import RSA
+
+
+PRIVATE_KEY_PW = "Alice"
+PASSWORD = "Bob"
+
+
+def run_test():
+    # Alice
+    key_alice = Key(AES256)
+    keys = Keys(RSA)
+    private, public, _ = keys.generate(PRIVATE_KEY_PW)
+    # Bob
+    key_bob = Key(AES256)
+    keys_bob = Keys(RSA)
+    bob_exchange = key_bob.generate(pw=PASSWORD, salt=public)
+    symmetric_key = keys_bob.encrypt(bob_exchange.key, public)
+    # Alice
+    alice_exchange_key = keys.decrypt(encrypted=symmetric_key, key=private, pw=PRIVATE_KEY_PW)
+    encrypted = key_alice.encrypt("Hello Bob", alice_exchange_key)
+    # Bob
+    message = key_bob.decrypt(encrypted, bob_exchange.key)
+    print(message)
