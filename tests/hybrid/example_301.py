604eb29ae09620d2711baa2c9d132559cab057d1
Eugen <eugen.hoppe.de@gmail.com>
2024-04-19 09:26:24 +0200

    bob and alice example

--- tests/hybrid/example_301.py
+++ tests/hybrid/example_301.py
@@ -1,28 +1,92 @@
+from dataclasses import dataclass
+
 from core.asymmetric.encryption import Keys
 from core.symmetric.encryption import Key
 from core.algorithms.aes import AES256
 from core.algorithms.rsa import RSA
 
 
-PRIVATE_KEY_PW = "Alice"
-PASSWORD = "Bob"
+
+MSG_1_HELLO_BOB = "Hello Bob"
+
+
+@dataclass
+class Person:
+    keys_asymmetric: Keys | None = None
+    key_symmetric: Key | None = None
+    key_exchange: str | None = None
+
+
+@dataclass
+class Alice(Person):
+    password: str = "Alice1995"
+    public_key: str | None = None
+    private_key: str | None = None
+    signature: str | None = None
+
+
+@dataclass
+class Bob(Person):
+    password: str = "Bob$123"
 
 
 def run_test():
-    # Alice
-    key_alice = Key(AES256)
-    keys = Keys(RSA)
-    private, public, _ = keys.generate(PRIVATE_KEY_PW)
-    # Bob
-    key_bob = Key(AES256)
-    keys_bob = Keys(RSA)
-    bob_exchange = key_bob.generate(pw=PASSWORD, salt=public)
-    symmetric_key = keys_bob.encrypt(bob_exchange.key, public)
-    # Alice
-    alice_exchange_key = keys.decrypt(
-        encrypted=symmetric_key, key=private, pw=PRIVATE_KEY_PW
+
+    # 1. Alice
+    # ========
+    alice = Alice()
+    alice.keys_asymmetric, alice.key_symmetric = Keys(RSA), Key(AES256)
+
+    alice.private_key, alice.public_key, _ = (
+        alice.keys_asymmetric.generate(pw=alice.password)
     )
-    encrypted = key_alice.encrypt("Hello Bob", alice_exchange_key)
-    # Bob
-    message = key_bob.decrypt(encrypted, bob_exchange.key)
-    print(message)
+
+    # 2. Bob
+    # ======
+    bob = Bob()
+    bob.keys_asymmetric, bob.key_symmetric = Keys(RSA), Key(AES256)
+
+    exchange = bob.key_symmetric.generate(
+        pw=bob.password,
+        salt=alice.public_key
+    )
+    bob.key_exchange = exchange.key
+    symmetric_key = bob.keys_asymmetric.encrypt(
+        payload=bob.key_exchange,
+        key=alice.public_key
+    )
+
+    # 3. Alice
+    # ========
+    alice.key_exchange = alice.keys_asymmetric.decrypt(
+        encrypted=symmetric_key,
+        key=alice.private_key,
+        pw=alice.password
+    )
+    encrypted_message = alice.key_symmetric.encrypt(
+        payload=MSG_1_HELLO_BOB,
+        key=alice.key_exchange
+    )
+    alice.signature = alice.keys_asymmetric.sign(
+        private_key_pem=alice.private_key,
+        message=MSG_1_HELLO_BOB,
+        pw=alice.password
+    )
+
+    # 4. Bob
+    # ======
+    decrypted_message = bob.key_symmetric.decrypt(
+        payload=encrypted_message,
+        key=bob.key_exchange
+    )
+    is_from_alice = bob.keys_asymmetric.validate(
+        public_key_pem=alice.public_key,
+        message=decrypted_message,
+        signature=alice.signature
+    )
+
+    if decrypted_message == MSG_1_HELLO_BOB:
+        if is_from_alice is True:
+            print(decrypted_message)
+        else:
+            assert False
