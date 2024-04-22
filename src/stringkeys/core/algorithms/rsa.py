6a1e46ed76e66e6d83b1bb61262252dc02edca40
Eugen <eugen.hoppe.de@gmail.com>
2024-04-22 23:39:22 +0200

    cc dry

--- src/stringkeys/core/algorithms/rsa.py
+++ src/stringkeys/core/algorithms/rsa.py
@@ -2,6 +2,7 @@ import base64
 
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives.asymmetric.types import PrivateKeyTypes
 from cryptography.hazmat.primitives.serialization import (
     Encoding,
     PrivateFormat,
@@ -23,24 +24,23 @@ class RSA(AsymmetricEncryption):
             public_exponent=options.key_public_exponent,
             key_size=options.key_size,
         )
-        public_key = private_key.public_key()
         if options.key_gen_private_key_pw:
             encryption_algorithm = BestAvailableEncryption(
                 options.key_gen_private_key_pw.encode()
             )
         else:
             encryption_algorithm = NoEncryption()
-        private_key_pem = private_key.private_bytes(
+        private_key_bytes = private_key.private_bytes(
             encoding=Encoding.PEM,
             format=PrivateFormat.PKCS8,
             encryption_algorithm=encryption_algorithm,
         )
-        public_key_pem = public_key.public_bytes(
+        public_key_bytes = private_key.public_key().public_bytes(
             encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo
         )
-        return private_key_pem.decode(), public_key_pem.decode()
+        return private_key_bytes.decode(), public_key_bytes.decode()
 
-    def encrypt(self, public_key: str, payload: str):
+    def encrypt(self, public_key: str, payload: str) -> str:
         cipher = load_pem_public_key(public_key.encode()).encrypt(
             payload.encode(),
             padding.OAEP(
@@ -51,13 +51,8 @@ class RSA(AsymmetricEncryption):
         )
         return base64.b64encode(cipher).decode("utf-8")
 
-    def decrypt(self, private_key: str, cipher: str, pw: str = None):
-        private_key_ = load_pem_private_key(  # DRY 1
-            private_key.encode(),
-            password=(pw.encode() if pw else None),
-            backend=None,
-        )
-        payload = private_key_.decrypt(
+    def decrypt(self, private_key: str, cipher: str, pw: str = None) -> str:
+        payload = RSA.pem_private_key(private_key, pw).decrypt(
             base64.b64decode(cipher),
             padding.OAEP(
                 mgf=padding.MGF1(algorithm=hashes.SHA256()),
@@ -68,12 +63,7 @@ class RSA(AsymmetricEncryption):
         return payload.decode()
 
     def sign(self, private_key: str, message: str, pw: str | None = None) -> str:
-        private_key_ = load_pem_private_key(  # DRY 1
-            private_key.encode(),
-            password=(pw.encode() if pw else None),
-            backend=None,
-        )
-        signature = private_key_.sign(
+        signature = RSA.pem_private_key(private_key, pw).sign(
             message.encode(),
             padding.PSS(
                 mgf=padding.MGF1(hashes.SHA256()), salt_length=padding.PSS.MAX_LENGTH
@@ -96,3 +86,12 @@ class RSA(AsymmetricEncryption):
             return True
         except InvalidSignature:
             return False
+
+    @staticmethod
+    def pem_private_key(private_key: str, pw: str | None) -> PrivateKeyTypes:
+        loaded_private_key = load_pem_private_key( 
+            private_key.encode(),
+            password=(pw.encode() if isinstance(pw, str) else None),
+            backend=None,
+        )
+        return loaded_private_key
